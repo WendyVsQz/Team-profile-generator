fe4f2e63f8ee2be91497fef0f1660c5399787d7e
Wendy <118736912+WendyVsQz@users.noreply.github.com>
2023-03-04 14:11:08 +0000

    initial point

---
+++ starter/lib/Employee.js
@@ -0,0 +1,40 @@
+// TODO: Write code to define and export the Employee class
+
+/* 
+The first class is an `Employee` parent class with the following properties and methods:       
+      * `name`
+      * `id`
+      * `email`
+      * `getName()`
+      * `getId()`
+      * `getEmail()`
+      * `getRole()`&mdash;returns `'Employee'`     
+      * The other three classes will extend `Employee`. 
+                                                         */
+class Employee {
+      constructor(name, id, email) {
+            this.name = name;
+            this.id = id;
+            this.email = email;
+      }
+
+//getName
+
+getName() {
+      return this.name;
+}
+
+
+//getID
+
+
+
+//getEmail
+
+
+
+//getRole
+
+
+}
+module.exports = Employee;
