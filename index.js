00db86322f75936c1553914316a872ab4e1368f2
Wendy <118736912+WendyVsQz@users.noreply.github.com>
2023-03-06 13:28:59 +0000

    testing

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
-const inquirer = require("inquirer");
-const fs = require("fs");
 // Team profiles
+const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const Employee = require("./lib/Employee");
-const path = require("path");
 
+// Modules
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+
+// variables used in the fs file function to create html
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html"); 
-
-
 const generateHTML = require("./src/page-template");
+const { default: Prompt } = require("inquirer/lib/prompts/base");
 
+// Empty array
+
+let staffData = [];
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
@@ -26,10 +31,10 @@ const generateHTML = require("./src/page-template");
                 { name: 'id', message: '123', initial: ''},
             {   type: "list", message: "what is your role?", 
                 choices:["Engineer", "intern", "Manager"]},
-        ]
-    },
+                ]
+        },
     {
-        name: 'Employee',
+        name: 'Manager',
         message: 'Please provide the following information:',
         choices: [
             { name: 'firstname', message: 'first name', initial: '' },
@@ -43,12 +48,15 @@ const generateHTML = require("./src/page-template");
 ]);
     
 
-
-prompt.run()
+inquirer
+.prompt.run()
 .then(value => console.log('Answer:', value))
 .catch(console.error);
 console.log('\x1b[36m%s\x1b[0m',prompt)
 
+function writeToFile(fileName, prompt) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), prompt);
+}
 /* // 2. if Manager ..
 const prompt = new Form({
     name: 'Employee',
@@ -97,20 +105,7 @@ const prompt = new Form({
     ]
 }); */
 
-prompt.run()
-.then(value => console.log('Answer:', value))
-.catch(console.error);
-console.log('\x1b[36m%s\x1b[0m',prompt)
-
-
-
-
 // 2. Create a function calling team members 
-
-
-
-
-
 
 function init() {
     console.log(
